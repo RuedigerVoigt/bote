f1957cd8558e721ef646c69636211ff9cdc0f05b
Rüdiger Voigt <ruedigervoigt@gmx.net>
2021-06-17 23:24:28 +0200

    Update version of dependency "userprovided"

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=["compatibility>=0.9.0",
-                      "userprovided>=0.9.0"],
+                      "userprovided>=0.9.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",

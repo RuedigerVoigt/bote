77f4a0d10d612e941c46fb92c9b8a984f8d96810
Rüdiger Voigt <ruedigervoigt@gmx.net>
2021-07-01 22:50:52 +0200

    bugfix: here also get version info from file

--- bote/__init__.py
+++ bote/__init__.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from bote.__main__ import Mailer
+from bote import _version
 
 NAME = "bote"
-__version__ = "1.0.0"
+__version__ = f"{_version.__version__}"
 __author__ = "Rüdiger Voigt"

822cb4a9d9921c9ef9956215588144edcc12751d
Rüdiger Voigt <ruedigervoigt@gmx.net>
2021-10-10 20:07:21 +0200

    Update dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=["compatibility>=1.0.1",
-                      "userprovided>=0.9.3"],
+                      "userprovided>=0.9.4"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",

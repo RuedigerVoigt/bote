06a3f38aa22f3920eeb78bc1e5bf4914d5745bba
Rüdiger Voigt <ruedigervoigt@gmx.net>
2025-11-08 19:34:19 +0100

    Remove shebang and encoding from most files

--- bote/__main__.py
+++ bote/__main__.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 """ Send email """
 
 from email.message import EmailMessage
--- bote/_version.py
+++ bote/_version.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 "Provide a single source of information"
 
 from datetime import date
